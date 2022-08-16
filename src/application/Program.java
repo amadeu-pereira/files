c1549edbfc6b1061d8637265b704bdbadb667551
Amadeu Pereira <amadeu.pereira.104@ufrn.edu.br>
2022-08-16 20:53:01 -0300

    Acrescentado criação de pastas

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,28 +1,37 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+	   Scanner sc = new Scanner (System.in);
 	   
-    String [] lines = new String[] {"good Morning", "Good Afternoon", "Good Night"};
-    
-    String path = "C:\\Users\\User\\Documents\\out.txt";
-    
-    try (BufferedWriter bw = new BufferedWriter(new FileWriter(path,true))){
-    	
-    	for (String line : lines) {
-    		bw.write(line);
-    		bw.newLine();
-    	}
-    } 
-    catch (IOException e) {
-    	e.printStackTrace();
-    }
+	   
+	   System.out.println("Enter a folder path: ");
+	   String srtPath = sc.nextLine();
+	   
+	   File path = new File(srtPath);
+	   
+	   File[] folders = path.listFiles(File::isDirectory);
+			   System.out.println("FOLDERS");
+	   for(File folder : folders) {
+		   System.out.println(folder);
+	   }
+			   
+	   File[] files = path.listFiles(File::isFile);
+	   System.out.println("Files");
+	   for(File file : files) {
+		   System.out.println(file);
+	   }
+	   
+	   boolean success = new File(srtPath + "\\Test").mkdir();
+	   System.out.println("Directory created successfully: " + success);
+	   
+	   sc.close();
+   
 	}
-
+	
 }
 
